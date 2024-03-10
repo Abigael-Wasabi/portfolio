12c4b1e15d43c9ee014699877b2828baf1383e80
Abiwasabi <abiwasabi03@gmail.com>
2024-03-10 16:52:58 +0300

    added sneaker web

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
     <>
       <Row style={{textAlign: 'center', display: 'flex', alignItems: 'center',justifyContent: 'center'}}>
           <b>MY BLUEPRINTS</b>
-          <li><a target='_blank' href="https://swiftpark-u-imain.vercel.app/" rel="noreferrer">(COOL KICKS)sneaker shop</a></li><br></br>
+          <li><a target='_blank' href="https://coolkicks.netlify.app/" rel="noreferrer">(COOL KICKS)sneaker shop</a></li><br></br>
           <li><a target='_blank' href="https://swiftpark-u-imain.vercel.app/" rel="noreferrer">VEHICLE PARKING MANAGEMENT SYSTEM</a></li><br></br>
           <li><a href="whta">WHTA NETWORKS WEBSITE</a><text>(still under development)</text></li>
         </Row>
