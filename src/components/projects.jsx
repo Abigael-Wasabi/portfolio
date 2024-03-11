f88c1a6ca0cd646bef02cb9b68de19261ab9f264
Abiwasabi <abiwasabi03@gmail.com>
2024-03-12 01:26:43 +0300

    added sneaker website

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,7 +8,9 @@ function Projects() {
           <b>MY BLUEPRINTS</b>
           <li><a target='_blank' href="https://coolkicks-sneaker-lhx1b92l6-abigael-wasabis-projects.vercel.app/" rel="noreferrer">(COOL KICKS)sneaker shop</a></li><br></br>
           <li><a target='_blank' href="https://swiftpark-u-imain.vercel.app/" rel="noreferrer">VEHICLE PARKING MANAGEMENT SYSTEM</a></li><br></br>
-          <li><a href="whta">WHTA NETWORKS WEBSITE</a><text>(still under development)</text></li>
+          <li><a href="whta">WHTA NETWORKS WEBSITE</a><text></text></li>
+          (all are still in development)
+          <pp style={{fontSize: '12px', marginTop: '100px'}}><i>reach out incase of a glitch</i></pp>
         </Row>
     </>
   )
