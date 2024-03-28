53a981c9cd7fabceed89cc81c9a6eea94795ea9f
Abiwasabi <abiwasabi03@gmail.com>
2024-03-28 12:36:24 +0300

    added whta web

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,8 +8,9 @@ function Projects() {
           <b>MY BLUEPRINTS</b>
           <li><a target='_blank' href="https://kicksemporium.netlify.app" rel="noreferrer">(COOL KICKS)sneaker shop</a></li><br></br>
           <li><a target='_blank' href="https://swiftpark-u-imain.vercel.app/" rel="noreferrer">VEHICLE PARKING MANAGEMENT SYSTEM</a></li><br></br>
-          <li><a href="whta">WHTA NETWORKS WEBSITE</a><text></text></li>
-          (all are still in development)
+          <li><a target='_blank' href="https://whtanetworks.com/" rel="noreferrer">WHTA NETWORKS WEBSITE</a>(in collaboration with other developers)</li>
+          <br></br>
+          (ALL ARE STILL IN DEVELOPMENT)
           <pp style={{fontSize: '12px', marginTop: '100px'}}><i>reach out incase of a glitch</i></pp>
         </Row>
     </>
